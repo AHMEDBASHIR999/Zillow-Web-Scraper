c81f2d5b9fad3e6e2b2d1bb3184c402f4d37d855
AHMEDBASHIR999 <147585277+AHMEDBASHIR999@users.noreply.github.com>
2024-05-30 17:41:00 +0500

    Update Motor_Status.py

--- views/Motor_Status.py
+++ views/Motor_Status.py
@@ -1,13 +1,13 @@
-import streamlit as st
+# import streamlit as st
 
-def load_view(status):
-    st.title('Motor Status: ')
-    if (status ==1):
-        st.write(f"<h3 style='text-align: center;'>Motor is HEALTHY </h1>", unsafe_allow_html=True)
-    elif(status==2):
-        st.write(f"<h3 style='text-align: center;'>Motor is UN-HEALTHY </h1>", unsafe_allow_html=True)
-    else:
-        st.write(f"<h3 style='text-align: center;'>Please Apply Machine and Deep Learning Model Firstly </h1>", unsafe_allow_html=True)
+# def load_view(status):
+#     st.title('Motor Status: ')
+#     if (status ==1):
+#         st.write(f"<h3 style='text-align: center;'>Motor is HEALTHY </h1>", unsafe_allow_html=True)
+#     elif(status==2):
+#         st.write(f"<h3 style='text-align: center;'>Motor is UN-HEALTHY </h1>", unsafe_allow_html=True)
+#     else:
+#         st.write(f"<h3 style='text-align: center;'>Please Apply Machine and Deep Learning Model Firstly </h1>", unsafe_allow_html=True)
         
 
     
@@ -16,11 +16,11 @@ def load_view(status):
 
 
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# hide_st_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             header {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_st_style, unsafe_allow_html=True)
