97eebdb931eedfdf8d50a0ee1e3eaf3dcb062cf2
AHMEDBASHIR999 <147585277+AHMEDBASHIR999@users.noreply.github.com>
2024-05-31 00:45:37 +0500

    Update Signal_processing.py

--- views/Signal_processing.py
+++ views/Signal_processing.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import undetected_chromedriver as uc
+import undetected_chromedriver.v2 as uc
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -11,7 +11,6 @@ import urllib.request
 import zipfile
 import os
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium import webdriver
 
 def install_chrome():
     # Download and install Google Chrome
@@ -271,5 +270,4 @@ def load_view():
     if st.session_state.scraped_data is not None:
         csv = st.session_state.scraped_data.to_csv(index=False)
         st.download_button(label="Download data as CSV", data=csv, file_name='property_data.csv', mime='text/csv')
-
 
